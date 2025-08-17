07653c871e68416c57e5d893cf38398b210cb17d
Diva <148589294+diva22001@users.noreply.github.com>
2025-08-18 01:14:07 +0800

    Update rpgl.sh

--- rpgl.sh
+++ rpgl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-# Install required Python packages
+# Install dependensi
+sudo apt-get update -y
+sudo apt-get install -y rclone  # Install Rclone
 pip install requests
-pip install huggingface-hub
 
-# Download rapidgator.py and run it
+# Unduh dan jalankan rapidgator.py
 curl -o rapidgator.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/rapidgator.py
 python rapidgator.py
 
-# Download huggingface.py and run it
-curl -o huggingface.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/huggingface.py
-python huggingface.py
+# Konfigurasi Rclone dan upload ke Google Drive
+rclone copy ./download gdrive:remote_folder -P  # Ganti 'remote_folder' dengan nama folder tujuan
